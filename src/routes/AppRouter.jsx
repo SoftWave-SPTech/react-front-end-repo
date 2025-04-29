e011a57d2c8bffbdcd79aeac5631df4ffd43200a
Brzenit <bryan.lima@sptech.school>
2025-04-29 01:08:27 -0300

    Ajuste arquivos errados

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
-import SiteInstitucional from "../pages/siteInstitucional/siteInstitucional";
+import SiteInstitucional from "../pages/siteInstitucional/SiteInstitucional";
 import CadastrarUsuarios from "../pages/CadastrarUsuario/CadastrarUsuarios";
 import Login from "../pages/login-primeiro-acesso/Login";
 import PrimeiroAcesso from "../pages/login-primeiro-acesso/PrimeiroAcesso";
