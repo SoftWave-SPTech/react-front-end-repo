99969473efd540fc31c2d0860f36f463b78e0dc1
Brzenit <bryan.lima@sptech.school>
2025-04-29 01:02:03 -0300

    exemplo de request com authorization no Form Advogado

--- src/components/AdvogadoFisicoForm.jsx
+++ src/components/AdvogadoFisicoForm.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { buscarCep } from '../service/buscarCep';
-import { v4 as uuidv4 } from 'uuid'; 
+import { v4 as uuidv4 } from 'uuid';
 import Inputmask from 'inputmask';
 
 export default function AdvogadoFisicoForm() {
@@ -95,7 +95,7 @@ export default function AdvogadoFisicoForm() {
     }
     if (formData.oab && !/^\d{6}$/.test(formData.oab)) {
       newErrors.oab = "OAB inválida. Deve conter exatamente 6 números.";
-    }    
+    }
 
     setErrors(newErrors);
     return Object.keys(newErrors).length === 0;
@@ -109,7 +109,12 @@ export default function AdvogadoFisicoForm() {
     const dadosParaEnviar = { ...formData, senha: novaSenha };
 
     console.log('Dados do formulário:', dadosParaEnviar);
-    axios.post('http://localhost:8080/advogados-fisicos', dadosParaEnviar)
+    // TODO ESCREVER AUTORIZACAO PARA AS OUTRAS REQUESTS
+    axios.post('http://localhost:8080/advogados-fisicos', dadosParaEnviar, {
+      headers: {
+        Authorization:`Bearer ${sessionStorage.getItem('token')}`,
+    },
+    })
       .then((response) => {
         console.log('Resposta:', response.data);
         alert('Cadastro realizado com sucesso!');
@@ -124,114 +129,114 @@ export default function AdvogadoFisicoForm() {
     <form className="formulario" onSubmit={handleSubmit}>
       <div className="coluna">
         <label>Nome:</label>
-        <input 
-          type="text" 
-          name="nome" 
-          placeholder="Digite o nome completo" 
-          value={formData.nome} 
-          onChange={handleChange} 
+        <input
+          type="text"
+          name="nome"
+          placeholder="Digite o nome completo"
+          value={formData.nome}
+          onChange={handleChange}
         />
         {errors.nome && <span className="error">{errors.nome}</span>}
 
         <label>CPF:</label>
-        <input 
-          type="text" 
-          name="cpf" 
-          placeholder="000.000.000-00" 
-          value={formData.cpf} 
-          onChange={handleChange} 
+        <input
+          type="text"
+          name="cpf"
+          placeholder="000.000.000-00"
+          value={formData.cpf}
+          onChange={handleChange}
         />
         {errors.cpf && <span className="error">{errors.cpf}</span>}
 
         <label>RG:</label>
-        <input 
-          type="text" 
-          name="rg" 
-          placeholder="00.000.000-0" 
-          value={formData.rg} 
-          onChange={handleChange} 
+        <input
+          type="text"
+          name="rg"
+          placeholder="00.000.000-0"
+          value={formData.rg}
+          onChange={handleChange}
         />
         {errors.rg && <span className="error">{errors.rg}</span>}
 
         <label>Email:</label>
-        <input 
-          type="email" 
-          name="email" 
-          placeholder="ana@email.com" 
-          value={formData.email} 
-          onChange={handleChange} 
+        <input
+          type="email"
+          name="email"
+          placeholder="ana@email.com"
+          value={formData.email}
+          onChange={handleChange}
         />
         {errors.email && <span className="error">{errors.email}</span>}
 
         <label>OAB:</label>
-        <input 
-          type="text" 
-          name="oab" 
-          placeholder="000000" 
-          value={formData.oab} 
-          onChange={handleChange} 
+        <input
+          type="text"
+          name="oab"
+          placeholder="000000"
+          value={formData.oab}
+          onChange={handleChange}
         />
         {errors.oab && <span className="error">{errors.oab}</span>}
 
         <label>Telefone:</label>
-        <input 
-          type="text" 
-          name="telefone" 
-          placeholder="(11) 98030-3049" 
-          value={formData.telefone} 
-          onChange={handleChange} 
+        <input
+          type="text"
+          name="telefone"
+          placeholder="(11) 98030-3049"
+          value={formData.telefone}
+          onChange={handleChange}
         />
         {errors.telefone && <span className="error">{errors.telefone}</span>}
       </div>
 
       <div className="coluna">
         <label>CEP:</label>
-        <input 
-          type="text" 
-          name="cep" 
-          placeholder="00000-000" 
-          value={formData.cep} 
-          onChange={handleChange} 
+        <input
+          type="text"
+          name="cep"
+          placeholder="00000-000"
+          value={formData.cep}
+          onChange={handleChange}
         />
         {errors.cep && <span className="error">{errors.cep}</span>}
 
         <label>Logradouro:</label>
-        <input 
-          type="text" 
-          name="logradouro" 
-          placeholder="Rua Exemplo" 
-          value={formData.logradouro} 
-          onChange={handleChange} 
+        <input
+          type="text"
+          name="logradouro"
+          placeholder="Rua Exemplo"
+          value={formData.logradouro}
+          onChange={handleChange}
         />
         {errors.logradouro && <span className="error">{errors.logradouro}</span>}
 
         <label>Bairro:</label>
-        <input 
-          type="text" 
-          name="bairro" 
-          placeholder="Centro" 
-          value={formData.bairro} 
-          onChange={handleChange} 
+        <input
+          type="text"
+          name="bairro"
+          placeholder="Centro"
+          value={formData.bairro}
+          onChange={handleChange}
         />
         {errors.bairro && <span className="error">{errors.bairro}</span>}
 
         <label>Cidade:</label>
-        <input 
-          type="text" 
-          name="cidade" 
-          placeholder="Cidade" 
-          value={formData.cidade} 
-          onChange={handleChange} 
+        <input
+          type="text"
+          name="cidade"
+          placeholder="Cidade"
+          value={formData.cidade}
+          onChange={handleChange}
         />
         {errors.cidade && <span className="error">{errors.cidade}</span>}
 
         <label>Complemento:</label>
-        <input 
-          type="text" 
-          name="complemento" 
-          placeholder="Apartamento, bloco, etc." 
-          value={formData.complemento} 
-          onChange={handleChange} 
+        <input
+          type="text"
+          name="complemento"
+          placeholder="Apartamento, bloco, etc."
+          value={formData.complemento}
+          onChange={handleChange}
         />
       </div>
 
