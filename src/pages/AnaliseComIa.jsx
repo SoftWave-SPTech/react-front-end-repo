7dd2be94ced6069fd60860ca040228e163fae216
Lauriano2003 <cristhian.marqueze@sptech.school>
2025-05-13 06:53:54 -0300

    tela de análise de processo

--- src/pages/AnaliseComIa.jsx
+++ src/pages/AnaliseComIa.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import LayoutBase from '../layouts/LayoutBase';
+import AnaliseMovimentacao from "../Components/AnaliseDeIa/AnaliseMovimentacao";
 
 const VisualizarDocumentosProcesso = () => 
 {
   return (
     <LayoutBase tipoMenu="cliente">
-      Seu conteúdo aqui
+      <AnaliseMovimentacao/>
     </LayoutBase>
   );
 };
