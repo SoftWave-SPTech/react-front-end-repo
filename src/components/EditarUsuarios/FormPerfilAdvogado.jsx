2bca75773ac71b36bb0abd5006810d8bcf7ae886
anacampaner <ana.fsilva@sptech.school>
2025-06-15 17:56:15 -0300

    ajuste merge

--- src/components/EditarUsuarios/FormPerfilAdvogado.jsx
+++ src/components/EditarUsuarios/FormPerfilAdvogado.jsx
@@ -275,22 +275,10 @@ function FormPerfilAdvogado() {
             arquivoFormatado.append("fotoPerfil", file);
 
             api.put(`${URLFOTO}/${sessionStorage.getItem('id')}`, arquivoFormatado, {
-<<<<<<< campos-formulario-editar
-                headers: {
+                headers: 
+                {
                     "Authorization": TOKEN
                 }
-=======
-            headers: {
-                "Authorization":  TOKEN
-            }
-            })
-            .then(response => {
-            console.log("Upload realizado com sucesso:", response.data);
-            
-            alert("Upload realizado com sucesso!");
-              sessionStorage.setItem('fotoPerfil', response.data);
-              window.location.reload()
->>>>>>> release/sprint-3
             })
                 .then(response => {
                     console.log("Upload realizado com sucesso:", response.data);
