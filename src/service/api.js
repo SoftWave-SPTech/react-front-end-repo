300454a8faa977a46b3913d52ca164226f02e831
Lua ♡ <160372370+lualuaSantosS2@users.noreply.github.com>
2025-12-06 01:08:31 -0300

    Update api.js

--- src/service/api.js
+++ src/service/api.js
@@ -1,17 +1,17 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://${EC2_IP_A}:8080',
+    baseURL: 'http://44.215.111.241:8080',
 });
 
 export const apiGemini = axios.create({
-    baseURL: 'http://${EC2_IP_A}:8082',
+    baseURL: 'http://44.215.111.241:8082',
 });
 
 export const apiInfoSimples = axios.create({
-    baseURL: 'http://${EC2_IP_A}:8084',
+    baseURL: 'http://44.215.111.241:8084',
 });
 
 export const apiAuthEmail = axios.create({
-    baseURL: 'http://${EC2_IP_A}:8080',
+    baseURL: 'http://44.215.111.241:8080',
 });
