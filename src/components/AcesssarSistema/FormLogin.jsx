8654660917ea826625ee1d99fcb927a4d7fececd
letfonsecaa <leticia.santos@sptech.school>
2025-12-07 14:41:23 -0300

    fix: add caminho correto

--- src/components/AcesssarSistema/FormLogin.jsx
+++ src/components/AcesssarSistema/FormLogin.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { api, apiAuthEmail } from '../../service/api';
 import Botao from '../Ui/Botao';
 import { Input } from '../Ui/Input';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import AlertStyle from '../Ui/AlertStyle';
 import boneco from '../../assets/images/boneco.png'; // ajuste o caminho conforme a estrutura do seu projeto
 
@@ -11,6 +11,7 @@ export default function FormLogin() {
     const [senha, setsenha] = useState("");
     const [errors, setErrors] = useState({});
     const [alert, setAlert] = useState({ show: false, message: '', type: 'error' });
+    const navigate = useNavigate();
 
     const validarFormulario = () => {
         const novosErros = {};
@@ -64,11 +65,11 @@ export default function FormLogin() {
 
                 setTimeout(() => {
                     if (response.data.role === "ROLE_USUARIO") {
-                        window.location.href = "/perfil-cliente";
+                         navigate ("/perfil-cliente");
                     } else if(response.data.role === "ROLE_ADMIN") {
-                        window.location.href = "/dashboard";
+                             navigate("/dashboard");
                     } else {
-                        window.location.href = "/perfil-advogado";
+                        navigate("/perfil-advogado");
                     }
                 }, 1200); 
             }
